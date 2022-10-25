e0d36d6394948317e497b37a94228331149c270f
Zeynep Sarıkaya <108154999+ToKyOzY@users.noreply.github.com>
2022-10-25 22:19:32 +0300

    Update ExecuteUpdate01.java

--- src/main/java/jdbc/ExecuteUpdate01.java
+++ src/main/java/jdbc/ExecuteUpdate01.java
@@ -6,7 +6,7 @@ public class ExecuteUpdate01 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
         Class.forName("org.postgresql.Driver");
-        Connection con= DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed","postgres","06Ankara06");
+        Connection con= DriverManager.getConnection("jdbc:postgresql://localhost:5432/techproed","postgres","****");
         Statement st=con.createStatement();
 
         //1. Örnek: number_of_employees değeri ortalama çalışan sayısından az olan
